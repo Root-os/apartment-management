38d456f7f915b579c5cbdca540818a28a0f55576
Salhe123 <salheseid92@gmail.com>
2025-02-21 10:52:31 +0300

    tenat paymen get

--- src/pages/protected/TenantBillView.js
+++ src/pages/protected/TenantBillView.js
@@ -1,18 +1,18 @@
 import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { setPageTitle } from '../../features/common/headerSlice'
-import AddBillPayment from '../../features/tenant-bill-payment/addTenantBillPayment'
+import ViewBillPayment from '../../features/tenant-bill-payment/viewTenantBills'
 
 function InternalPage(){
     const dispatch = useDispatch()
 
     useEffect(() => {
-        dispatch(setPageTitle({ title : "Add Tenant"}))
+        dispatch(setPageTitle({ title : "Get Tenant Payment"}))
       }, [])
 
 
     return(
-        <AddBillPayment />
+        <ViewBillPayment />
     )
 }
 
