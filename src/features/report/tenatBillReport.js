98eb7936dd6000da1cf5e7d9753d89b6bf380ddd
Salhe123 <salheseid92@gmail.com>
2025-02-22 09:39:02 +0300

    tenant bill payment report done

--- src/features/report/tenatBillReport.js
+++ src/features/report/tenatBillReport.js
@@ -1,158 +1,172 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import TableComponent from '../../components/table'
+import TableComponent from '../../components/table';
+import Modal from '../../components/Modal';
 
 const TenantBillReport = () => {
+  const [tenantPayments, setTenantPayments] = useState([]);
+  const [tenants, setTenants] = useState([]);
+  const [billTypes, setBillTypes] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalMessage, setModalMessage] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
+  const [filterParams, setFilterParams] = useState({
+    startDate: "",
+    endDate: "",
+    billPaymentTypeId: "",
+    tenantId: ""
+  });
 
-    const [floors, setFloors] = useState([]);
-    const [isEditModalOpen, setIsEditModalOpen] = useState(false);
-    const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
-    const [selectedFloor, setSelectedFloor] = useState(null);
-    const [newFloorData, setNewFloorData] = useState({
-      name: '',
-      totalUnits: '',
-      rentedUnits: '',
-      freeUnits: ''
-    });
-  
-  
-    // Fetch floors
-    useEffect(() => {
-      axios.get(`${process.env.REACT_APP_BASE_URL}tenant-payments/reports`)
-        .then(response => {
-          setFloors(response.data);
-        })
-        .catch(error => {
-          console.error("Error fetching floors:", error);
-        });
-    }, []);
-  
-    // Edit floor
-    const handleEditClick = (floor) => {
-      setSelectedFloor(floor);
-      setNewFloorData({
-        name: floor.name,
-        totalUnits: floor.totalUnits,
-        rentedUnits: floor.rentedUnits,
-        freeUnits: floor.freeUnits,
-      });
-      setIsEditModalOpen(true);
-    };
-  
-    const handleEditSubmit = () => {
-      axios.put(`${process.env.REACT_APP_BASE_URL}tenant-payments/${selectedFloor.id}`, newFloorData)
-        .then(() => {
-          setFloors(floors.map(floor => (floor.id === selectedFloor.id ? { ...floor, ...newFloorData } : floor)));
-          setIsEditModalOpen(false);
-        })
-        .catch(error => {
-          console.error("Error updating floor:", error);
-        });
-    };
-  
-    // Delete floor
-    const handleDeleteClick = (floor) => {
-      setSelectedFloor(floor);
-      setIsDeleteModalOpen(true);
-    };
-  
-    const handleDeleteConfirm = () => {
-      axios.delete(`${process.env.REACT_APP_BASE_URL}tenant-payments/${selectedFloor.id}`)
-        .then(() => {
-          setFloors(floors.filter(floor => floor.id !== selectedFloor.id));
-          setIsDeleteModalOpen(false);
-        })
-        .catch(error => {
-          console.error("Error deleting floor:", error);
-        });
-    };
-  
-    const columns = [
-      { key: 'name', label: 'Name' },
-      { key: 'totalUnits', label: 'Total Units' },
-      { key: 'rentedUnits', label: 'Rented Units' },
-      { key: 'freeUnits', label: 'Free Units' },
-      {
-        key: 'actions',
-        label: 'Actions',
-        render: (floor) => (
-          <>
-            <button
-              onClick={() => handleEditClick(floor)}
-              className="bg-blue-500 text-white py-1 px-4 rounded mr-2"
-            >
-              Detail
-            </button>
-           
-          </>
-        )
+  // Fetch Tenants and Bill Types
+  useEffect(() => {
+    const fetchTenants = async () => {
+      try {
+        const response = await axios.get(`${process.env.REACT_APP_BASE_URL}tenant`);
+        setTenants(response.data);
+      } catch (error) {
+        console.error("Error fetching tenants:", error);
       }
-    ];
-  
-    const handleAddClick = () => {
-        console.log("Add button clicked");
-      };
-      
-  
-    return (
-      <div className="p-8">
-        <TableComponent
-          title="Floor List"
-          data={floors}
-          columns={columns}
-          rowsPerPageOptions={[5, 10, 15]}
-          showSearch={true}
-          exportable={true}
-          onAdd={handleAddClick}
-        />
-  
-        {/* Edit Modal */}
-        {isEditModalOpen && (
-          <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
-            <div className="bg-base-100 p-6 rounded-lg w-96">
-              <h2 className="text-xl mb-4">Edit Floor</h2>
-              <div className="mb-4">
-                <label className="block text-sm font-medium mb-2">Floor Name</label>
-                <input
-                  type="text"
-                  value={newFloorData.name}
-                  onChange={(e) => setNewFloorData({ ...newFloorData, name: e.target.value })}
-                  className="bg-base-100 w-full p-2 border border-gray-300 rounded"
-                />
-              </div>
-              <div className="mb-4">
-                <label className="block text-sm font-medium mb-2">Total Units</label>
-                <input
-                  type="number"
-                  value={newFloorData.totalUnits}
-                  onChange={(e) => setNewFloorData({ ...newFloorData, totalUnits: e.target.value })}
-                  className="bg-base-100 w-full p-2 border border-gray-300 rounded"
-                />
-              </div>
-              <div className="flex justify-between">
-                <button onClick={() => setIsEditModalOpen(false)} className="bg-gray-400 text-white px-4 py-2 rounded">Cancel</button>
-                <button onClick={handleEditSubmit} className="bg-blue-500 text-white px-4 py-2 rounded">Save</button>
-              </div>
-            </div>
+    };
+
+    const fetchBillTypes = async () => {
+      try {
+        const response = await axios.get(`${process.env.REACT_APP_BASE_URL}bill-type`);
+        setBillTypes(response.data);
+      } catch (error) {
+        console.error("Error fetching bill types:", error);
+      }
+    };
+
+    fetchTenants();
+    fetchBillTypes();
+  }, []);
+
+  const handleFilterSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true); // Start loading
+
+    try {
+      const response = await axios.post(`${process.env.REACT_APP_BASE_URL}tenant-payments/reports`, filterParams);
+      setFilteredData(response.data);
+    } catch (error) {
+      const message = error.response?.status === 404
+        ? "No payments found with the given filters"
+        : "Error filtering data. Please try again.";
+      setModalMessage(message);
+      setIsModalOpen(true);
+      console.error("Error filtering data:", error);
+    } finally {
+      setIsLoading(false); // Stop loading
+    }
+  };
+
+  const columns = [
+    { key: 'tenantName', label: 'Tenant Name', render: (payment) => payment.Tenant.fullName },
+    { key: 'paymentType', label: 'Payment Type Name', render: (payment) => payment.BillPaymentType.typeName },
+
+    { key: 'billTypeName', label: 'Bill Type', render: (payment) => payment.BillPaymentType.typeName },
+    { key: 'amount', label: 'Amount' },
+    { key: 'startDate', label: 'Start Date', render: (payment) => new Date(payment.startDate).toLocaleDateString() },
+    { key: 'endDate', label: 'End Date', render: (payment) => new Date(payment.endDate).toLocaleDateString() },
+    { key: 'status', label: 'Status' }
+  ];
+
+  return (
+    <div className="p-8">
+      <div className="container mx-auto p-4">
+        <h2 className="text-2xl font-bold mb-6">Tenant Bill Report</h2>
+
+        <form onSubmit={handleFilterSubmit} className="grid grid-cols-4 gap-4">
+          {/* Start Date */}
+          <div>
+            <label htmlFor="startDate" className="block text-sm font-medium text-gray-700 dark:text-gray-300">Start Date</label>
+            <input
+              type="date"
+              id="startDate"
+              className="w-full p-2 border border-gray-300 rounded dark:bg-gray-800 dark:border-gray-700 dark:text-gray-300"
+              value={filterParams.startDate}
+              onChange={(e) => setFilterParams({ ...filterParams, startDate: e.target.value })}
+            />
           </div>
-        )}
-  
-        {/* Delete Confirmation Modal */}
-        {isDeleteModalOpen && (
-          <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
-            <div className="bg-white p-6 rounded-lg w-96">
-              <h2 className="text-xl mb-4">Are you sure you want to delete this floor?</h2>
-              <div className="flex justify-between">
-                <button onClick={() => setIsDeleteModalOpen(false)} className="bg-gray-400 text-white px-4 py-2 rounded">Cancel</button>
-                <button onClick={handleDeleteConfirm} className="bg-red-500 text-white px-4 py-2 rounded">Delete</button>
-                
-              </div>
-            </div>
+
+          {/* End Date */}
+          <div>
+            <label htmlFor="endDate" className="block text-sm font-medium text-gray-700 dark:text-gray-300">End Date</label>
+            <input
+              type="date"
+              id="endDate"
+              className="w-full p-2 border border-gray-300 rounded dark:bg-gray-800 dark:border-gray-700 dark:text-gray-300"
+              value={filterParams.endDate}
+              onChange={(e) => setFilterParams({ ...filterParams, endDate: e.target.value })}
+            />
           </div>
-        )}
+
+          {/* Tenant */}
+          <div>
+            <label htmlFor="tenantId" className="block text-sm font-medium text-gray-700 dark:text-gray-300">Tenant</label>
+            <select
+              id="tenantId"
+              className="w-full p-2 border border-gray-300 rounded dark:bg-gray-800 dark:border-gray-700 dark:text-gray-300"
+              value={filterParams.tenantId}
+              onChange={(e) => setFilterParams({ ...filterParams, tenantId: e.target.value })}
+            >
+              <option value="">Select Tenant</option>
+              {tenants.map((tenant) => (
+                <option key={tenant.id} value={tenant.id}>{tenant.fullName}</option>
+              ))}
+            </select>
+          </div>
+
+          {/* Bill Type */}
+          <div>
+            <label htmlFor="billPaymentTypeId" className="block text-sm font-medium text-gray-700 dark:text-gray-300">Bill Type</label>
+            <select
+              id="billPaymentTypeId"
+              className="w-full p-2 border border-gray-300 rounded dark:bg-gray-800 dark:border-gray-700 dark:text-gray-300"
+              value={filterParams.billPaymentTypeId}
+              onChange={(e) => setFilterParams({ ...filterParams, billPaymentTypeId: e.target.value })}
+            >
+              <option value="">Select Bill Type</option>
+              {billTypes.map((type) => (
+                <option key={type.id} value={type.id}>{type.typeName}</option>
+              ))}
+            </select>
+          </div>
+
+          {/* Submit Button */}
+          <div className="col-span-4 flex justify-end">
+            <button
+              type="submit"
+              className="w-40 bg-blue-500 text-white p-2 rounded hover:bg-blue-700 dark:bg-blue-700 dark:text-gray-300"
+            >
+              {isLoading ? "Processing..." : "Filter Data"}
+            </button>
+          </div>
+        </form>
       </div>
-    );
+
+      <TableComponent
+        title="Filtered Tenant Bill Report"
+        data={filteredData}
+        columns={columns}
+        rowsPerPageOptions={[5, 10, 15]}
+        showSearch={true}
+        exportable={true}
+      />
+
+      {/* Modal for displaying error message */}
+      {isModalOpen && (
+        <Modal
+          isOpen={isModalOpen}
+          onClose={() => setIsModalOpen(false)}
+          type="error"
+          message={modalMessage}
+        />
+      )}
+    </div>
+  );
 };
 
 export default TenantBillReport;
