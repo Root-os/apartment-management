0fd7b223424223aa26502184a922c86854f8b5ca
Salhe123 <salheseid92@gmail.com>
2025-03-07 12:08:49 +0300

    dashboard initialization

---
+++ src/features/dashboard/components/diagram.js
@@ -0,0 +1,111 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import * as d3 from "d3";
+
+const UnitStatusReport = () => {
+  const [data, setData] = useState({
+    totalOccupied: 0,
+    totalAvailable: 0,
+    totalUnderMaintenance: 0,
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const baseUrl = process.env.REACT_APP_BASE_URL;
+        const response = await axios.get(`${baseUrl}unit/status/data/report`);
+        if (response.data.success) {
+          setData(response.data.data);
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    if (data.totalOccupied || data.totalAvailable || data.totalUnderMaintenance) {
+      const dataset = [
+        { label: `Occupied: ${data.totalOccupied}`, value: data.totalOccupied, color: "red" },
+        { label: `Available: ${data.totalAvailable}`, value: data.totalAvailable, color: "green" },
+        { label: data.totalUnderMaintenance > 0 ? `Maintenance: ${data.totalUnderMaintenance}` : '', value: data.totalUnderMaintenance, color: "blue" },
+      ];
+
+      const filteredDataset = dataset.filter(d => d.value > 0);
+
+      const width = 200;
+      const height = 200;
+      const radius = Math.min(width, height) / 2;
+
+      const svg = d3.select("#pie-chart")
+        .attr("width", width)
+        .attr("height", height)
+        .append("g")
+        .attr("transform", `translate(${width / 2}, ${height / 2})`);
+
+      const pie = d3.pie()
+        .value(d => d.value)
+        .sort(null);
+
+      const arc = d3.arc()
+        .innerRadius(0)
+        .outerRadius(radius);
+
+      const path = svg.selectAll("path")
+        .data(pie(filteredDataset))
+        .enter()
+        .append("path")
+        .attr("d", arc)
+        .attr("fill", d => d.data.color);
+
+      const text = svg.selectAll("text")
+        .data(pie(filteredDataset))
+        .enter()
+        .append("text")
+        .attr("transform", d => `translate(${arc.centroid(d)})`)
+        .attr("dy", "0.35em")
+        .style("text-anchor", "middle")
+        .style("fill", "#fff")
+        .style("font-size", "12px")
+        .text(d => d.data.value);
+
+      // Adding legend
+      const legend = d3.select("#legend")
+        .attr("width", width)
+        .attr("height", 50)
+        .selectAll("g")
+        .data(filteredDataset)
+        .enter()
+        .append("g")
+        .attr("transform", (d, i) => `translate(0, ${i * 20})`);
+
+      legend.append("rect")
+        .attr("x", 0)
+        .attr("y", 0)
+        .attr("width", 18)
+        .attr("height", 18)
+        .style("fill", d => d.color);
+
+      legend.append("text")
+        .attr("x", 24)
+        .attr("y", 9)
+        .attr("dy", "0.35em")
+        .style("text-anchor", "start")
+        .style("font-size", "12px")
+        .text(d => d.label.split(":")[0]); // Displaying only the name part
+
+    }
+  }, [data]);
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-6 dark:bg-slate-900 dark:text-white">
+      <div className="pb-56 w-full max-w-md">
+        <svg id="pie-chart"></svg>
+        <svg id="legend"></svg>
+      </div>
+    </div>
+  );
+};
+
+export default UnitStatusReport;
--- src/features/dashboard/index.js
+++ src/features/dashboard/index.js
@@ -1,27 +1,45 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { FaBell, FaCreditCard, FaExclamationCircle } from 'react-icons/fa';
+import {
+  FaBell,
+  FaCreditCard,
+  FaExclamationCircle,
+  FaExpand,
+  FaCompress,
+  FaBuilding,
+  FaUserFriends,
+  FaCar,
+  FaDollarSign,
+  FaEnvelope,
+  FaUser,
+  FaDolly,
+  FaMoneyBillAlt,
+  FaFileInvoiceDollar,
+  FaHome,FaQuestionCircle
+} from 'react-icons/fa';
+import UnitStatusReport from './components/diagram';
 
 const Dashboard = () => {
   const [counts, setCounts] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState('');
+  const [isWrapped, setIsWrapped] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const token = localStorage.getItem('token'); 
+        const token = localStorage.getItem('token');
         const response = await axios.get(`${process.env.REACT_APP_BASE_URL}dashboard`, {
           headers: {
-            Authorization: `Bearer ${token}`, 
+            Authorization: `Bearer ${token}`,
           },
         });
-        setCounts(response.data); 
+        setCounts(transformData(response.data));
       } catch (error) {
         setError('Error loading data: ');
         console.error('Error fetching data');
       } finally {
-        setLoading(false); 
+        setLoading(false);
       }
     };
 
@@ -37,11 +55,101 @@ const Dashboard = () => {
     return color;
   };
 
-  // Icons mapping based on the counts data
+  const transformData = (data) => {
+    const transformedData = {};
+    for (const [key, value] of Object.entries(data)) {
+      transformedData[key] = {};
+      for (const [subKey, subValue] of Object.entries(value)) {
+        transformedData[key][keyMapping[subKey]] = subValue;
+      }
+    }
+    return transformedData;
+  };
+
+  const keyMapping = {
+    totalNotifications: 'Total Notifications',
+    sentNotifications: 'Sent Notifications',
+    readNotifications: 'Read Notifications',
+    totalPaymentsRequest: 'Total Payments Request',
+    pendingPaymentsRequest: 'Pending Payments Request',
+    completedPaymentsRequest: 'Completed Payments Request',
+    totalComplaints: 'Total Complaints',
+    inProgressComplaints: 'In-Progress Complaints',
+    resolvedComplaints: 'Resolved Complaints',
+    notResolvedComplaints: 'Not Resolved Complaints',
+    totalUnits: 'Total Units',
+    availableUnits: 'Available Units',
+    occupiedUnits: 'Occupied Units',
+    underMaintenanceUnits: 'Under Maintenance Units',
+    totalFloors: 'Total Floors',
+    availableFloors: 'Available Floors',
+    underMaintenanceFloors: 'Under Maintenance Floors',
+    totalTenants: 'Total Tenants',
+    activeTenants: 'Active Tenants',
+    inactiveTenants: 'Inactive Tenants',
+    terminatedTenants: 'Terminated Tenants',
+    totalVehicles: 'Total Vehicles',
+    totalInventory: 'Total Inventory',
+    moveInInventories: 'Move-In Inventories',
+    moveOutInventories: 'Move-Out Inventories',
+    totalParking: 'Total Parking',
+    onParking: 'On Parking',
+    readyToOut: 'Ready To Out',
+    completed: 'Completed',
+    totalExpenses: 'Total Expenses',
+    totalItems: 'Total Items',
+    totalPurchasedItems: 'Total Purchased Items',
+    totalExistedItems: 'Total Existed Items',
+    alertNumberOfItems: 'Alert Number Of Items',
+    totalWithdrawals: 'Total Withdrawals',
+    pendingWithdrawals: 'Pending Withdrawals',
+    approvedWithdrawals: 'Approved Withdrawals',
+    rejectedWithdrawals: 'Rejected Withdrawals',
+    processedWithdrawals: 'Processed Withdrawals',
+    totalEmails: 'Total Emails',
+    sentEmails: 'Sent Emails',
+    readEmails: 'Read Emails',
+    totalEmployees: 'Total Employees',
+    adminEmployees: 'Admin Employees',
+    totalSalaries: 'Total Salaries',
+    pendingSalaries: 'Pending Salaries',
+    paidSalaries: 'Paid Salaries',
+    totalStockouts: 'Total Stockouts',
+    pendingStockouts: 'Pending Stockouts',
+    completedStockouts: 'Completed Stockouts',
+    rejectedStockouts: 'Rejected Stockouts',
+    totalTenantPayments: 'Total Tenant Payments',
+    pendingTenantPayments: 'Pending Tenant Payments',
+    paidTenantPayments: 'Paid Tenant Payments',
+    overdueTenantPayments: 'Overdue Tenant Payments',
+    totalBillPayments: 'Total Bill Payments',
+    pendingBillPayments: 'Pending Bill Payments',
+    paidBillPayments: 'Paid Bill Payments',
+    overdueBillPayments: 'Overdue Bill Payments',
+    totalRentCollections: 'Total Rent Collections',
+    paidRentCollections: 'Paid Rent Collections',
+    pendingRentCollections: 'Pending Rent Collections',
+    overdueRentCollections: 'Overdue Rent Collections',
+  };
+
   const iconMapping = {
-    notifications: <FaBell size={30} />,
-    payments: <FaCreditCard size={30} />,
-    complaints: <FaExclamationCircle size={30} />,
+    notifications: <FaBell size={30} title="Notifications" />,
+    paymentsRequest: <FaCreditCard size={30} title="Payments Request" />,
+    complaints: <FaExclamationCircle size={30} title="Complaints" />,
+    units: <FaBuilding size={30} title="Units" />,
+    tenants: <FaUserFriends size={30} title="Tenants" />,
+    tenantVehicles: <FaCar size={30} title="Tenant Vehicles" />,
+    expenses: <FaDollarSign size={30} title="Expenses" />,
+    emails: <FaEnvelope size={30} title="Emails" />,
+    employees: <FaUser size={30} title="Employees" />,
+    stockouts: <FaDolly size={30} title="Stockouts" />,
+    tenantPayments: <FaMoneyBillAlt size={30} title="Tenant Payments" />,
+    billPayments: <FaFileInvoiceDollar size={30} title="Bill Payments" />,
+    rentCollections: <FaHome size={30} title="Rent Collections" />,
+  };
+
+  const handleWrapToggle = () => {
+    setIsWrapped(!isWrapped);
   };
 
   if (loading) {
@@ -52,73 +160,54 @@ const Dashboard = () => {
   }
 
   return (
-    <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-3 2xl:gap-7.5">
-      {counts && (
-        <>
-          {/* Notifications Card */}
-          <div
-            className="card hover:shadow-xl transition-all transform hover:scale-105"
-            style={{ backgroundColor: generateRandomColor() }}
-          >
-            <div className="card-body p-6 mt-10">
-              <div className="flex items-center space-x-4">
-                <div className="flex-shrink-0">
-                  <div className="text-black">{iconMapping.notifications}</div>
-                </div>
-                <div>
-                  <h3 className="text-xl font-semibold text-black">Notifications</h3>
-                  <p className="text-lg text-black">
-                     {counts.notifications.totalNotifications} 
-                     {/* | Sent: {counts.notifications.sentNotifications} | Read: {counts.notifications.readNotifications} */}
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
+    <div className="container mx-auto p-4">
+      <div className="flex justify-end">
+        <button onClick={handleWrapToggle} className="text-xl">
+          {isWrapped ? <FaCompress /> : <FaExpand />}
+        </button>
+      </div>
+      <div
+        className={`grid gap-4 ${
+          isWrapped ? 'grid-cols-1' : 'grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-3'
+        }`}
+      >
+       {counts && (
+  <>
+    {Object.keys(counts).map((key) => (
+      <div
+        key={key}
+        className="card hover:shadow-xl transition-all transform hover:scale-105 relative"
+        style={{ backgroundColor: generateRandomColor() }}
+      >
+        <div className="absolute top-2 left-2 flex items-center space-x-2">
+          {/* Ensure iconMapping[key] is not undefined */}
+          <div>{iconMapping[key] || <FaQuestionCircle size={30} title="Unknown" />}</div>
+          
+          {/* Ensure keyMapping[key] is not undefined */}
+          <h3 className="text-xl font-semibold text-black">
+            {keyMapping[key] || key} {/* Fallback to the key if no mapping found */}
+          </h3>
+        </div>
+        <div className="card-body p-6 mt-10">
+          <ul className="text-sm text-black">
+            {Object.keys(counts[key]).map((subKey) => (
+              <li key={subKey}>
+                {subKey}: {counts[key][subKey]}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    ))}
+  </>
+)}
 
-          {/* Payments Card */}
-          <div
-            className="card hover:shadow-xl transition-all transform hover:scale-105"
-            style={{ backgroundColor: generateRandomColor() }}
-          >
-            <div className="card-body p-6 mt-10">
-              <div className="flex items-center space-x-4">
-                <div className="flex-shrink-0">
-                  <div className="text-black">{iconMapping.payments}</div>
-                </div>
-                <div>
-                  <h3 className="text-xl font-semibold text-black">Payments</h3>
-                  <p className="text-lg text-black">
-                   {counts.payments?.totalPayments} 
-                   {/* | Pending: {counts.payments.pendingPayments} | Completed: {counts.payments.completedPayments} */}
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
+      </div>
+      <hr className="my-6 border-t-2 border-dotted border-gray-500 dark:border-gray-300" />
 
-          {/* Complaints Card */}
-          <div
-            className="card hover:shadow-xl transition-all transform hover:scale-105"
-            style={{ backgroundColor: generateRandomColor() }}
-          >
-            <div className="card-body p-6 mt-10">
-              <div className="flex items-center space-x-4">
-                <div className="flex-shrink-0">
-                  <div className="text-black">{iconMapping.complaints}</div>
-                </div>
-                <div>
-                  <h3 className="text-xl font-semibold text-black">Complaints</h3>
-                  <p className="text-lg text-black">
-                  {counts.complaints.totalComplaints} 
-                    {/* | In Progress: {counts.complaints.inProgressComplaints} | Resolved: {counts.complaints.resolvedComplaints} | Not Resolved: {counts.complaints.notResolvedComplaints} */}
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </>
-      )}
+      <div className="w-full max-w-md">
+        <UnitStatusReport />
+      </div>
     </div>
   );
 };
