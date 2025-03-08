09a57f3d1a6b571b1c169f3c1cbbbfb3b9858d51
Salhe123 <salheseid92@gmail.com>
2025-03-08 09:27:09 +0300

    table adjustment done

--- src/components/table.js
+++ src/components/table.js
@@ -83,6 +83,7 @@ const TableComponent = ({
   // Define padding based on the selected density
   const rowPadding = density === 'comfortable' ? 'py-4' : 'py-2';
 
+
   return (
     <div className="p-6 bg-base-100 rounded-lg shadow-md w-full overflow-x-auto">
       {/* Title Card */}
@@ -91,10 +92,10 @@ const TableComponent = ({
         <div className="flex items-center space-x-2">
           {onAdd && (
             <button 
-              className="px-2 py-1 bg-blue-500 text-white rounded-lg hover:bg-blue-600 text-sm"
+              className="flex items-center space-x-1 px-3 py-1 bg-blue-500 text-white rounded-lg hover:bg-blue-600 text-sm transition-all duration-300"
               onClick={onAdd}
             >
-              <FaPlus /> Add
+              <FaPlus className="text-lg" /> <span>Add</span>
             </button>
           )}
           {exportable && (
@@ -102,26 +103,28 @@ const TableComponent = ({
               <CSVLink
                 data={data}
                 filename={`${title}.csv`}
-                className="px-2 py-1 bg-green-500 text-white rounded-md hover:bg-green-600 text-sm"
+                className="flex items-center space-x-1 px-3 py-1 bg-green-500 text-white rounded-md hover:bg-green-600 text-sm transition-all duration-300"
               >
-                <FaDownload /> Export CSV
+                <FaDownload className="text-lg" /> <span>Export CSV</span>
               </CSVLink>
               <button
-                className="px-2 py-1 bg-red-500 text-white rounded-lg hover:bg-red-600 text-sm"
+                className="flex items-center space-x-1 px-3 py-1 bg-red-500 text-white rounded-lg hover:bg-red-600 text-sm transition-all duration-300"
                 onClick={handleExportPDF}
               >
-                <FaDownload /> Export PDF
+                <FaDownload className="text-lg" /> <span>Export PDF</span>
               </button>
               <button
-                className="px-2 py-1 bg-yellow-500 text-white rounded-lg hover:bg-yellow-600 text-sm"
+                className="flex items-center space-x-1 px-3 py-1 bg-yellow-500 text-white rounded-lg hover:bg-yellow-600 text-sm transition-all duration-300"
                 onClick={handlePrint}
               >
-                <FaPrint /> Print
+                <FaPrint className="text-lg" /> <span>Print</span>
               </button>
             </>
           )}
         </div>
       </div>
+    
+  
 
       <div className="flex justify-between items-center mb-4">  
       {showSearch && (
@@ -173,6 +176,7 @@ const TableComponent = ({
           ))}
         </tbody>
       </table>
+
 
       {/* Pagination */}
       <div className="flex justify-between items-center mt-4">
